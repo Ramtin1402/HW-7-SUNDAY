e5e543b183f18c71ce5d074196f96488c9568b1a
Ramtin <149162748+Ramtin1402@users.noreply.github.com>
2023-12-31 12:43:22 +0330

    Add files via upload

---
+++ HW 7 (2) sunday.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a , b , c;
+	cin >> a;
+	cin >> b;
+	for (c=0 ; b<=a ; c++){
+		a =a - b;
+		}
+		cout << "Baqi mande :"
+	<< a;
+		cout << "kharej qesmat :"
+	<< c;
+	}
+
